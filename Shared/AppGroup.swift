c39d63579074a3dd36bd4f8edea7919cf64fd051
imarues <rhom.adel1212@gmail.com>
2026-09-05 19:13:07 +0300

    Add shared app group storage

---
+++ Shared/AppGroup.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+enum AppGroup {
+    static let identifier = "group.com.ikira.keyboard"
+    static var defaults: UserDefaults {
+        UserDefaults(suiteName: identifier) ?? .standard
+    }
+}
+
+enum SharedKeys {
+    static let clipboardItems = "clipboard.items.v2"
+    static let clipboardInsertion = "clipboard.insertion"
+    static let currentLanguage = "keyboard.currentLanguage"
+    static let learnedWords = "keyboard.learnedWords"
+    static let hapticsEnabled = "keyboard.haptics"
+}
